22f5bd6dd250e55bdd4649e7ca3c2f8ef8b0d9c7
tommy8699 <tomashresko700@gmail.com>
2021-11-23 00:09:45 +0100

    Add Post/Category grid

--- app/Model/Repositories/PostCategoryRepository.php
+++ app/Model/Repositories/PostCategoryRepository.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace App\Model\Repositories;
 
-final class PostCategoryRepository
+final class PostCategoryRepository extends BaseRepository
 {
+
+    public static function getTableName(): string
+    {
+        return 'post_category';
+    }
+
 
 }
--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
-use Nette;
+use App\Components\Post\Category\Grid\PresenterTrait as PostCategoryGridPresenterTrait;
 use Nette\Application\UI\Presenter;
 
 
 final class HomepagePresenter extends Presenter
 {
+    use PostCategoryGridPresenterTrait;
 
+    public function actionDefault():void
+    {
+        $this->canCreatePostCategoryGridControl = true;
+    }
 }
